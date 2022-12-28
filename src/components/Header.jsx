0cc0c790de41975b54b99500a8cafd2f9eb235db
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-28 15:25:39 +0100

    ⏪️ Get back to npm instead of pnpm because of eslint errors ✨ New header for Main Layout, with Logo and nav bar. Non-functionnal navigation at the moment.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import '../styles/header.css'
+import logo from '../assets/logo_header.png'
 
 export default function Header () {
     return (
         <header className="main-header">
-
+            <img src={logo} alt="Logo du header de Kasa, écrit en rouge avec à la place du premier A une maison."/>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>À propos</li>
+                </ul>
+            </nav>
         </header>
     )
 }
--- src/views/MainLayout.jsx
+++ src/views/MainLayout.jsx
@@ -1,9 +1,10 @@
 import {Outlet} from "react-router-dom";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function MainLayout () {
     return <main>
-        <h1>Hello World! (Main Layout)</h1>
+        <Header/>
         <Outlet/>
         <Footer/>
     </main>
