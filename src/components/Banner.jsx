4a415dfbd376bef2d21f11507e65302d97a0ff23
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-28 16:25:41 +0100

    💄 Finished Banner, with correct background color and opacity to look darken

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,12 @@
+import '../styles/banner.css'
+import banner_image from '../assets/banner.png'
+
 export default function Banner () {
-    return <div>Bonjour, ceci est une bannière</div>
+    return (
+        <div className="banner">
+            <img src={banner_image}
+                 alt="La bannière, on peut y voir des montagnes et des affleurements rocheux de près et quelques arbres"/>
+            <div>Chez vous, partout et ailleurs</div>
+        </div>
+    )
 }
