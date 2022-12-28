f8c509279a4fb433baa2073da529a1e04a572840
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-28 19:52:06 +0100

    💄 Created main layout for ErrorPage, to be replaced by real layout from Router once I know how to do

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,3 +1,12 @@
-export default function ErrorPage() {
-    return <h1>Error 404: Page Not Found</h1>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function ErrorPage () {
+    return (
+        <div>
+            <Header/>
+            <h1>Error 404: Page not found</h1>
+            <Footer/>
+        </div>
+    )
 }
