5bf988708ad68380ec41b068dc15080e30bc8218
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-29 13:36:16 +0100

    Added TODO Tags

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,9 +5,22 @@ const data = require('../datas/logements.json')
 console.log(data)
 
 export default function Gallery () {
+    // TODO: RenderCard Function
+    const renderCard = (title, src, description) => {
+        return (
+            <Card title={title} src={src} description={description}/>
+        )
+    }
+    // TODO: RenderCard Function
 
     return (
         <div className="gallery-container">
+            {
+                data.forEach((value) => {
+                    renderCard(value.title, value.cover, value.description)
+                    console.log('Coucou cest nous')
+                })
+            }
             <Card title={data[0].title} src={data[0].cover} description={data[0].description}/>
             <Card title={data[1].title} src={data[1].cover} description={data[1].description}/>
             <Card title={data[2].title} src={data[2].cover} description={data[2].description}/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,11 @@ export default function Header () {
             <img src={logo} alt="Logo du header de Kasa, écrit en rouge avec à la place du premier A une maison."/>
             <nav>
                 <ul>
-                    <li>Accueil</li>
+                    {/* TODO : React Router Link*/}
+                    <li>
+                        Accueil
+                    </li>
+                    {/* TODO : React Router Link*/}
                     <li>À propos</li>
                 </ul>
             </nav>
