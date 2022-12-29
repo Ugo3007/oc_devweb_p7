a6e0d90d5587a0c60a5d13e29616f97aa39dfb40
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-29 21:14:14 +0100

    ⚡️ Included Contact component + replaced fake values by placeholder coming from logement.json with real include 💄 Improved styling to match the style wanted

--- src/views/HousingDetails.jsx
+++ src/views/HousingDetails.jsx
@@ -1,20 +1,23 @@
 import '../styles/housing_details.css'
+import Contact from "../components/Housing/Contact";
 
-export default function HousingDetails (props) {
+const data = require('../datas/logements.json')
+export default function HousingDetails () {
     return (
         <div className="housing-details-container">
             {/* TODO: Carousel */}
             <div className="row">
                 <section className="column">
-                    <h2>{props.title}</h2>
-                    <p>{props.location}</p>
+                    <h2>{data[0].title}</h2>
+                    <p>{data[0].location}</p>
                 </section>
                 <section className="column">
-                    {/* TODO: Contact */}
+                    <Contact src={data[0].cover} hostname={data[0].host.name}/>
                 </section>
             </div>
             <div className="row">
                 <section className="column">
+                    <h2>Coucou</h2>
                     {/* TODO: Tag list */}
                 </section>
                 <section className="column">
