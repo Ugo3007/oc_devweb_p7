957cd2326e36e73dba57de8b90193b3df9006b87
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-29 18:41:31 +0100

    📝 Structured file with TODOs to visualize remaining work

--- src/views/HousingDetails.jsx
+++ src/views/HousingDetails.jsx
@@ -1,9 +1,30 @@
 import '../styles/housing_details.css'
 
-export default function HousingDetails () {
+export default function HousingDetails (props) {
     return (
         <div className="housing-details-container">
-            <h2>Housing details container</h2>
+            {/* TODO: Carousel */}
+            <div className="row">
+                <section className="column">
+                    <h2>{props.title}</h2>
+                    <p>{props.location}</p>
+                </section>
+                <section className="column">
+                    {/* TODO: Contact */}
+                </section>
+            </div>
+            <div className="row">
+                <section className="column">
+                    {/* TODO: Tag list */}
+                </section>
+                <section className="column">
+                    {/* TODO: Notation */}
+                </section>
+            </div>
+            <div className="row">
+                {/* TODO: Burger 1 = Description */}
+                {/* TODO: Burger 2 = Equipements */}
+            </div>
         </div>
     )
 }
