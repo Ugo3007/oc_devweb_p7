bf338964b2576e7461cf1d01f60fa8903f59c8ed
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2022-12-29 11:56:08 +0100

    💄 Dealt with ErrorPage design

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,11 +1,18 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+import '../styles/error_page.css'
+import error_logo from "../assets/error_page.png";
+
 export default function ErrorPage () {
     return (
-        <div>
+        <div className="error_page">
             <Header/>
-            <h1>Error 404: Page not found</h1>
+            <div className="main-container">
+                <img src={error_logo} alt="Logo 404, qui signifie que nous sommes arrivés sur une page d'erreur."/>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <p>Retourner sur la page d'accueil</p>
+            </div>
             <Footer/>
         </div>
     )
