a71ffec0902b32036982e3678176ce841a0010d2
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2023-01-04 14:36:04 +0100

    ✨💄 Stuffed About component with Collapsible components and added css to display properly

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,3 +1,17 @@
+import Collapsible from "../components/Housing/Collapsible";
+import '../styles/about.css'
+
 export default function About () {
-    return <h2>À Propos</h2>
+    const fiabilite = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    const respect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    const service = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    const securite = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    return (
+        <div className="about-container">
+            <Collapsible collapsibleTitle="Fiabilité" collapsibleContent={fiabilite}/>
+            <Collapsible collapsibleTitle="Respect" collapsibleContent={respect}/>
+            <Collapsible collapsibleTitle="Service" collapsibleContent={service}/>
+            <Collapsible collapsibleTitle="Sécurité" collapsibleContent={securite}/>
+        </div>
+    )
 }
