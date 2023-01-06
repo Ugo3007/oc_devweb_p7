249ec4d5a6c104f61e19e3612e88cdd73633cce8
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2023-01-06 14:44:57 +0100

    🐛 Corrected bug: Tags display were not dynamically functionnal

--- src/components/Housing/Tag.jsx
+++ src/components/Housing/Tag.jsx
@@ -2,6 +2,12 @@ import '../../styles/Housing/tag.css'
 
 export default function Tag ({tagname}) {
     return (
-        <div className="tag">{tagname}</div>
+        <div className='tag-container'>
+            {
+                tagname.map((value) => {
+                    return <div className="tag">{value}</div>
+                })
+            }
+        </div>
     )
 }
