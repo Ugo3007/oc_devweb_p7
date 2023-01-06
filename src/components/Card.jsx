76b449fbcc36b2ba5f844e389e8182890b8ed876
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2023-01-06 11:27:39 +0100

    ⚡️ Added the Link tag to make the card clickable and redirect to the accommodation page. In addition, we added the id of the accommodation in the uri to retrieve it in Housing details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 import '../styles/card.css'
+import {Link} from "react-router-dom";
 
 export default function Card (props) {
     return (
         <div className="card">
-            <div className="linear-gradiant"></div>
-            <img src={props.src} className="card__image" alt={props.description}/>
-            {/* Remplacer par une image quand on aura set les props*/}
-            <div className="title"><h3>{props.title}</h3></div>
+            <Link to={'/logement/' + props.id}>
+                <div className="linear-gradiant"></div>
+                <img src={props.src} className="card__image" alt={props.description}/>
+                {/* Remplacer par une image quand on aura set les props*/}
+                <div className="title"><h3>{props.title}</h3></div>
+            </Link>
         </div>
     )
 }
