5b4f48f3bacf138b62128279396fb9ec5de22af0
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2023-01-10 11:00:44 +0100

    ✨ Using NavLinks to improve UE during navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
 import '../styles/header.css'
 import logo from '../assets/logo_header.png'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function Header () {
+    let activeStyle = {
+        textDecoration: 'underline'
+    }
     return (
         <header className="main-header">
             <img src={logo} alt="Logo du header de Kasa, écrit en rouge avec à la place du premier A une maison."/>
             <nav>
                 <ul>
-                    <li><Link className='link' to='/'>Accueil</Link></li>
-                    <li><Link className='link' to='/a-propos'>A propos</Link></li>
+                    <li>
+                        <NavLink
+                            to='/'
+                            className='link'
+                            style={({isActive}) =>
+                                isActive ? activeStyle : undefined
+                            }
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/a-propos'
+                            className='link'
+                            style={({isActive}) =>
+                                isActive ? activeStyle : undefined
+                            }
+                        >
+                            A propos
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
