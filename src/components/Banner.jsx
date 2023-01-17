d63827392fc11777a605116d068a204d25ebd00a
GABRIEL Ugo <ugo.gabriel@etu.unistra.fr>
2023-01-17 11:11:28 +0100

    🐛 Fixed visual bugs that did not correspond with figma

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,16 @@ export default function Banner ({
                                     img_src,
                                     divText
                                 }) {
+
+    const texts = divText.split('/')
     return (
         <div className="banner">
             <img src={img_src}
                  alt="La bannière, on peut y voir des montagnes et des affleurements rocheux de près et quelques arbres"/>
-            <div>{divText}</div>
+            <div>
+                <p>{texts[0]}</p>
+                <p>{texts[1]}</p>
+            </div>
         </div>
     )
 }
--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -7,7 +7,7 @@ import banner_image from "../assets/banner.png";
 export default function Homepage () {
     return (
         <div className="homepage">
-            <Banner img_src={banner_image} divText='Chez vous, partout et ailleurs'/>
+            <Banner img_src={banner_image} divText='Chez vous, /partout et ailleurs'/>
             <Gallery/>
         </div>
     )
